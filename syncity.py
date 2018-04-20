5fabe3da837369ca038974d9033d79e66aa98ddb
Rodrigo Orph <rodrigo.orph@gmail.com>
2018-04-19 21:27:35 -0300

    autoversion update

--- syncity.py
+++ syncity.py
@@ -15,7 +15,7 @@ import random
 
 from syncity import common, settings_manager
 
-SYNCITY_VERSION = '18.04.12.2141'
+SYNCITY_VERSION = '18.04.13.1631'
 SIMULATOR_MIN_VERSION = '18.03.15.0000'
 
 print ('SynCity toolbox - v{}\nCopyright (c) 2016-{} CVEDIA PVE Ltd\n'.format(SYNCITY_VERSION, datetime.date.today().year))
